45f7e0531b0520f7358a4bf3d670da75bb2dfb27
tingwei628 <sbvq6p199xe@gmail.com>
2017-11-28 00:51:21 +0800

    add build

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
-import path from 'path';
 
 const config = {
+  input: "src/index.js",
   output: {
-    file: "",
+    file: "build/index.min.js",
     name: "rollup-plugin-hotreload",
     format: 'umd',
     sourcemap: true
@@ -15,7 +15,7 @@ const config = {
     resolve({
       jsnext: true,
       main: true,
-      browser: true,
+      browser: true
     }),
     commonjs(),
     babel({
@@ -28,19 +28,4 @@ const config = {
   ]
 };
 
-
-function bundle(config, outputpath, sourcefilepath) {
-  const output1 = Object.assign({}, config.output, {
-    file: path.join(__dirname, outputpath)
-  });
-  const module1 = Object.assign({}, config, {
-    input: path.join(__dirname, sourcefilepath),
-    output: output1,
-  });
-  return module1;
-}
-
-export default [
-  bundle(config, "build/index.min.js", "src/index.js"),
-  // bundle(config, "build/js/hotreload.min.js", "utils/hotreload.js")
-];
+export default config;
