7597ac863aaabd3518da5f8d36c76fd0d5942c64
tingwei628 <sbvq6p199xe@gmail.com>
2018-07-02 00:54:23 +0800

    remove socketio

---
+++ test/hotreload.test.js
@@ -0,0 +1,47 @@
+import hotreload from "../lib/hotreload";
+import socketio from "socketio";
+
+const serverForTest = http.createServer((req, res) => res.end());
+const messageForTest = {
+  reload: "reloadTest",
+  disconnect: "disconnectTest"
+}
+const portTest = 5000;
+
+const optionsForTest = {
+  server: serverForTest,
+  socketio: socketioTest,
+  port: portTest,
+  message: messageForTest
+};
+
+
+describe("test server", () => {
+  test("test options server", () => {
+
+  })
+});
+
+test("test whether server get started", () => {
+
+});
+
+// 
+
+// const onPress = jest.fn();
+// expect(onPress).toBeCalledWith(
+//     expect.objectContaining({
+//       x: expect.any(Number),
+//       y: expect.any(Number),
+//     }),
+//   );
+
+
+
+
+
+
+
+//test options were passed
+//test whether server got started
+//test whether socket.io connected with socket.io-client 
