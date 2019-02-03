5dce415fe78117c97fdd1cd2d2485970219b90f9
tingwei628 <sbvq6p199xe@gmail.com>
2019-02-03 10:39:16 +0800

    add more tests

--- __test__/rollup-plugin-hotreload.test.js
+++ __test__/rollup-plugin-hotreload.test.js
@@ -1,13 +1,18 @@
 import { rphMultibundles } from '../lib/rollup-plugin-hotreload';
-// test validate in multibundles
-// test expect copyTransform
+import {
+  createHashKey,
+  initHashKeyMap,
+  checkModuleHashKey,
+  processTemplateHtml,
+  pipeHotReloadJS,
+  checkInputOptions,
+  getCopyStrPaths,
+  createWatchFileServer,
+  responseCallback,
+  getBuildArr
+} from '../lib/helpers';
 
-// test pipe in read/write html
-// test write file to another file with string
-
-
-
-var config = {
+let config = {
   templateHtmlPath: "src/index.html",
   isStopRPH: false,
   rootDir: __dirname,
@@ -17,12 +22,64 @@ var config = {
     ["js/index2.min.js", "src/index2.js"]
   ]
 };
-var buildPaths = config.buildPaths;
-var dirname = __dirname;
+let buildPaths = config.buildPaths;
+let dirname = __dirname;
+
+
+/*
+  createHashKey,
+  initHashKeyMap,
+  checkModuleHashKey,
+  processTemplateHtml,
+  pipeHotReloadJS,
+  checkInputOptions,
+  getCopyStrPaths,
+  createWatchFileServer,
+  responseCallback,
+  getBuildArr
+
+*/
+
+describe('test checkInputOptions', () => {
+  test('test checkInputOptions - rooDir', () => {
+    let options = {};
+    expect(() => checkInputOptions(options)).toThrow('rootDir is needed!');
+  });
+  test('test checkInputOptions - rootBuildDir', () => {
+    let options = { rootDir: __dirname };
+    expect(() => checkInputOptions(options)).toThrow('rootBuildDir is needed!');
+  });
+  test('test checkInputOptions - buildPaths', () => {
+    let options = { rootDir: __dirname, rootBuildDir: 'build' };
+    expect(() => checkInputOptions(options)).toThrow('buildPaths must have at least ONE file!');
+  });
+  test('test checkInputOptions - templateHtmlPath', () => {
+    let options = {
+      rootDir: __dirname,
+      rootBuildDir: 'build',
+      buildPaths: [["js/index.min.js", "src/index.js"]]
+    }
+    expect(() => checkInputOptions(options)).toThrow('templatePath html is needed!');
+  });
+});
+
 
 
 test('test getMultibundles -> multiBundles', () => {
-  var result = rphMultibundles(config);
-  //expect(Array.isArray(result)).toEqual(true);
+  let result = rphMultibundles(config);
   expect(Array.isArray(result)).toBeTruthy;
+});
+
+
+describe('test HashKey related functions', () => {
+
+  beforeEach(function () {
+    jest.resetModules(); // reset import modules
+    jest.resetAllMocks(); // reset all mocks
+  });
+  test('test createHashKey contains exact 12 numbers', () => {
+    let result = createHashKey();
+    expect(result).toMatch(/^[a-z0-9]{12}$/);
+  });
+
 });
